4abaa4d694a18b71a22d3be60a2d9a2584efcb77
Olzen <ilmanaidil6@gmail.com>
2026-09-23 18:14:13 +0800

    test

--- app/src/main/java/com/lab/myfoodrescue/MainActivity.kt
+++ app/src/main/java/com/lab/myfoodrescue/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             MyFoodRescueTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "kff",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
