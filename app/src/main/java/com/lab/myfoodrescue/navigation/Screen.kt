d7fbadd86cf92768ebf0922e1821bbbcfdd17e9c
ArmourG2 <akmal2kembar@gmail.com>
2026-09-25 09:38:19 +0800

    restructure navHost.kt

--- app/src/main/java/com/lab/myfoodrescue/navigation/Screen.kt
+++ app/src/main/java/com/lab/myfoodrescue/navigation/Screen.kt
@@ -1,10 +1,16 @@
 package com.lab.myfoodrescue.navigation
 
 enum class Screen (val route: String){
-    REGISTER("register"),
+    ROLE_SELECTION("role_selection"),
     HOME("home"),
     SEARCH("search"),
+    LISTING_DETAIL("listing_detail"),
+    RESERVATION("reservation"),
+    PICKUP_SCHEDULE("pickup_schedule"),
+    STATUS("status"),
+    HISTORY("history"),
     PROFILE("profile"),
     SETTINGS("settings"),
+    CREATE_DONATION("create_donation")
 
 }
--- app/src/main/java/com/lab/myfoodrescue/navigation/navHost.kt
+++ app/src/main/java/com/lab/myfoodrescue/navigation/navHost.kt
@@ -3,15 +3,58 @@ package com.lab.myfoodrescue.navigation
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
 
 @Composable
-fun NavigationLogic(
+fun FoodRescueNavHost(
     navController: NavHostController
 ) {
     NavHost(
         navController = navController,
-        startDestination = Screen.REGISTER.route
+        startDestination = Screen.ROLE_SELECTION.route
     ) {
+        composable(Screen.ROLE_SELECTION.route) {
+            // TODO: Show RoleSelectionScreen
+        }
 
+        composable(Screen.HOME.route) {
+            // TODO: Show HomeScreen
+        }
+
+        composable(Screen.SEARCH.route) {
+            // TODO: Show SearchScreen
+        }
+
+        composable(Screen.LISTING_DETAIL.route) {
+            // TODO: Show ListingDetailScreen
+        }
+
+        composable(Screen.RESERVATION.route) {
+            // TODO: Show ReservationScreen
+        }
+
+        composable(Screen.PICKUP_SCHEDULE.route) {
+            // TODO: Show PickupScheduleScreen
+        }
+
+        composable(Screen.STATUS.route) {
+            // TODO: Show StatusScreen
+        }
+
+        composable(Screen.HISTORY.route) {
+            // TODO: Show HistoryScreen
+        }
+
+        composable(Screen.PROFILE.route) {
+            // TODO: Show ProfileScreen
+        }
+
+        composable(Screen.SETTINGS.route) {
+            // TODO: Show SettingsScreen
+        }
+
+        composable(Screen.CREATE_DONATION.route) {
+            // TODO: Show CreateDonationScreen
+        }
     }
 }
